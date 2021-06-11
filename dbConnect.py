cb9deb28e9fe6d43f0f958febfb78393da2d675b
YourPeppaPig <51061967+YourPeppaPig@users.noreply.github.com>
2021-06-11 22:47:02 +0300

    Config

---
+++ dbConnect.py
@@ -0,0 +1,9 @@
+class Settings:
+
+    # Database
+    host = 'database-1.cxuu6midraas.us-east-1.rds.amazonaws.com'
+    port = 5432
+    user = 'postgres'
+    password = 'postgres'
+    database = 'postgres'
+    # Database
---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings:
+
+    subjects_count = 98
+    rosstat_path = r'/root/files/rosstat'
+    emicc_path = r'/root/files/emicc'
+
+    # Database
+    host = 'database-1.cxuu6midraas.us-east-1.rds.amazonaws.com'
+    port = 5432
+    user = 'postgres'
+    password = 'postgres'
+    database = 'postgres'
+    # Database
